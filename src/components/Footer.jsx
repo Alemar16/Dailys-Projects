1cdff6996f7d34bfced02b5d5e4e178c0ab4cd9d
Armando Martinez <alemar.martinez16@gmail.com>
2023-10-15 00:01:57 -0300

    add Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="footer mt-auto py-3 bg-body-tertiary">
+      <div className="container">
+        <p className="float-end mb-1">
+          <a href="#">Back to top</a>
+        </p>
+        <span className="text-body-secondary">
+          Place sticky footer content here.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
