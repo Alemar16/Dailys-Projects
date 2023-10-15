e3b5cbe42ee3b69c379c8f4d3a67bc6797363cb6
Armando Martinez <alemar.martinez16@gmail.com>
2023-10-15 06:24:01 -0300

    update component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Navbar from "./components/navbar.jsx";
 import Footer from "./components/Footer.jsx";
-// import ExtensiveCards from "./Practice-Components/Extensive-Cards/React/ExtensiveCards.jsx";
+import ExtensiveCards from "../src/Practice-Components/Extensive-Cards/React/ExtensiveCards.jsx";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/extensive-cards" element={<ExtensiveCards />} /> */}
+        <Route path="/extensive-cards" element={<ExtensiveCards />} />
 
       </Routes>
       <Footer/>
