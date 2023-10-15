ee9303f184330e09606015a6b3328cbb2eaf9983
Armando Martinez <alemar.martinez16@gmail.com>
2023-10-15 06:17:33 -0300

    modifique footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 const Footer = () => {
   return (
-    <div className="footer mt-auto py-3 bg-body-tertiary">
+    <div className="footer mt-auto py-3 bg-body-tertiary" >
       <div className="container">
-        <p className="float-end mb-1">
-          <a href="#">Back to top</a>
-        </p>
+        
         <span className="text-body-secondary">
           Place sticky footer content here.
         </span>
+        <p className="float-end mb-1">
+          <a href="#">Back to top</a>
+        </p>
       </div>
     </div>
   );
