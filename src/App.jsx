51095bb217e92d34e3aef28ca9b4e8d9565d8ce8
Armando Martinez <alemar.martinez16@gmail.com>
2023-10-19 01:46:43 -0300

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
-import Navbar from "./components/navbar.jsx";
-import Footer from "./components/Footer.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import ExtensiveCards from "../src/Practice-Components/Extensive-Cards/React/ExtensiveCards.jsx";
+import ExtensiveCardsViews from "./components/views/Extensive-Cards/ExtensiveCardsViews.jsx";
 import "./App.css";
+import PropTypes from "prop-types";
+
+const LayoutWithNavbarAndFooter = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+LayoutWithNavbarAndFooter.propTypes = {
+  children: PropTypes.node,
+};
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/extensive-cards" element={<ExtensiveCards />} />
-
-      </Routes>
-      <Footer/>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <LayoutWithNavbarAndFooter>
+            <Home />
+          </LayoutWithNavbarAndFooter>
+        }
+      />
+      <Route
+        path="/extensive-cards"
+        element={
+          <LayoutWithNavbarAndFooter>
+            <ExtensiveCards />
+          </LayoutWithNavbarAndFooter>
+        }
+      />
+      <Route path="/extensive-cards-views" element={<ExtensiveCardsViews />} />
+    </Routes>
   );
 }
 
