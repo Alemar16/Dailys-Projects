94de40aef43e18cb507d913b52f09807ad452a64
Armando Martinez <alemar.martinez16@gmail.com>
2023-10-26 09:26:25 -0300

    create routes Random Gallery Images

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Navbar from "./components/navbar/Navbar.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import ExtensiveCards from "../src/Practice-Components/Extensive-Cards/React/ExtensiveCards.jsx";
 import ExtensiveCardsViews from "./components/views/Extensive-Cards/ExtensiveCardsViews.jsx";
+import RandomImageViews from "./components/views/Random-Image/RandomImageViews.jsx";
 import "./App.css";
 import PropTypes from "prop-types";
+import RandomImage from "./Practice-Components/Random-Image/React/RandomImage.jsx";
 
 const LayoutWithNavbarAndFooter = ({ children }) => {
   return (
@@ -40,7 +42,16 @@ function App() {
           </LayoutWithNavbarAndFooter>
         }
       />
+      <Route
+        path="/random-image"
+        element={
+          <LayoutWithNavbarAndFooter>
+            <RandomImage />
+          </LayoutWithNavbarAndFooter>
+        }
+      />
       <Route path="/extensive-cards-views" element={<ExtensiveCardsViews />} />
+      <Route path="/random-image-views" element={<RandomImageViews />} />
     </Routes>
   );
 }
