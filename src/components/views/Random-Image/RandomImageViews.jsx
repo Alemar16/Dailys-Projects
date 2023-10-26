09e8427e7a7414ddda339aa0c96a6749628239ba
Armando Martinez <alemar.martinez16@gmail.com>
2023-10-26 17:34:30 -0300

    add styles images + name user

--- src/components/views/Random-Image/RandomImageViews.jsx
+++ src/components/views/Random-Image/RandomImageViews.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { createApi } from "unsplash-js";
 import imageBg from "../../../Practice-Components/Random-Image/BasicAplication/images/pexel-bg.jpg";
 
-//import "./style.css"; // Importa el archivo de estilos CSS 
+import "./style.css"; // Importa el archivo de estilos CSS
 
 const unsplash = createApi({
   accessKey: import.meta.env.VITE_UNSPLASH_ACCESS_KEY,
@@ -12,10 +12,12 @@ const unsplash = createApi({
 const RandomImageViews = () => {
   const [images, setImages] = useState([]);
 
- useEffect(() => {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
-        const { response: images } = await unsplash.photos.getRandom({ count: 15 });
+        const { response: images } = await unsplash.photos.getRandom({
+          count: 15,
+        });
         setImages(images);
       } catch (error) {
         console.error("Error fetching images from Unsplash", error);
@@ -63,32 +65,31 @@ const RandomImageViews = () => {
               fontWeight: "bold",
               fontSize: "30px",
               color: "#222121",
-              maxWidth: "800px", 
+              maxWidth: "800px",
               lineHeight: 1.6,
               flex: 1,
             }}
           >
             Welcome to our{" "}
-            <span className="highlight"
+            <span
+              className="highlight"
               style={{
                 color: "#ca970a",
                 textShadow: "2px 2px 2px #000000",
                 letterSpacing: "2px",
               }}
             >
-              Random Image Gallery</span>.<br />{" "}
-            Explore our extensive collection of carefully curated high-quality
-            photographs to inspire and delight.
+              Random Image Gallery
+            </span>
+            .<br /> Explore our extensive collection of carefully curated
+            high-quality photographs to inspire and delight.
           </p>
           <div className="container-button">
             <a href="https://unsplash.com" target="_blank" rel="noreferrer">
-              <img className="logo-unsplash"
+              <img
+                className="logo-unsplash"
                 src="https://logowik.com/content/uploads/images/unsplash8609.jpg"
                 alt="Logo de Unsplash"
-                style={{
-                  height: "50px",
-                  width: "50px",
-                }}
               />
             </a>
             <button id="refreshButton">Refresh Images</button>
@@ -96,30 +97,14 @@ const RandomImageViews = () => {
         </div>
       </div>
 
-      <div
-        className="container"
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          maxWidth: "1100px",
-          border: "#333 1px solid",
-          borderRadius: "10px",
-          padding: "20px",
-          background: "rgba(255, 255, 255, 0.158)",
-          WebkitBackdropFilter: "blur(16px) saturate(180%)",
-          backdropFilter: "blur(16px) saturate(180%)",
-          boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
-          WebkitBackdropFilter: "blur(4px)",
-          backdropFilter: "blur(4px)",
-          borderRadius: "12px",
-          border: "1px rgba(255, 255, 213, 0.3209219) solid",
-        }}
-      >
+      <div className="container d-flex align-items-center justify-content-center justify-content-center flex-wrap p-4">
         <div className="row row-cols-3">
           {images.map((image) => (
-            <div className="col" key={image.id}>
+            <div
+              className="col"
+              key={image.id}
+              style={{ position: "relative", overflow: "hidden" }}
+            >
               <img
                 src={image.urls.regular}
                 alt={image.alt_description}
@@ -129,12 +114,24 @@ const RandomImageViews = () => {
                   height: "300px",
                   width: "300px",
                   maxWidth: "100%",
-                  borderRadius: "7%",
+                  borderRadius: "7px",
                   transition: "transform 0.3s",
                   cursor: "pointer",
                 }}
               />
-              <p>{image.alt_description}</p>
+              <div className="user-overlay ">
+                <a
+                  href={image.user.links.html}
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: "#ca970a",
+                  }}
+                >
+                  {image.user.name}
+                </a>
+              </div>
             </div>
           ))}
         </div>
@@ -156,11 +153,16 @@ const RandomImageViews = () => {
           <a
             href="https://www.linkedin.com/in/armando-mart%C3%ADnez-zambrano"
             target="_blank"
+            rel="noreferrer"
           >
             <ion-icon name="logo-linkedin"></ion-icon>
           </a>
           <p>Created by Alemar16 - © 2023</p>
-          <a href="https://github.com/Alemar16" target="_blank">
+          <a
+            href="https://github.com/Alemar16"
+            target="_blank"
+            rel="noreferrer"
+          >
             <ion-icon name="logo-github"></ion-icon>
           </a>
         </div>
